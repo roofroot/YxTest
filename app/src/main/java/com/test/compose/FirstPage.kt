f457ffce7db4784951ccca656a8b7b1df8c116d4
yuxiu <yuxiu5@faw.com.cn>
2023-04-26 21:14:07 +0800

    modified the ui

--- app/src/main/java/com/test/compose/FirstPage.kt
+++ app/src/main/java/com/test/compose/FirstPage.kt
@@ -4,20 +4,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import com.test.compose.ui.theme.bgColor
 
 /**
  * coco man
@@ -27,64 +34,84 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FristPage(list: ArrayList<String>) {
-    var content = remember { mutableStateOf("") }
-    var content2 = remember { mutableStateOf("") }
+    var content = remember { mutableStateOf<String?>(null) }
+    var content2 = remember { mutableStateOf<String?>(null) }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(bgColor)
     ) {
         InputView("amount:", content)
         InputView("time:", content2)
-        Text(text = "submit",
+        BoxWithConstraints(
             Modifier
-                .wrapContentWidth()
-                .height(20.dp)
-                .background(Color.Blue)
-                .clickable {
-                    content?.value?.let { num1 ->
-                        content2?.value?.let { num2 ->
-                            val result = num1.toInt() * num2.toInt()
-                            list.add("result:${result}")
-                            MainActivity.navigation.changeCurrentPage(PageName.SECONDPAGE)
-                        }
-                    }
-                })
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
+
+            Box(
+                Modifier
+                    .background(Color.Green, shape = RoundedCornerShape(10.dp))
+                    .align(Alignment.Center)
+            ) {
+                Text(text = "submit",
+                    Modifier
+                        .padding(10.dp).wrapContentSize()
+                        .align(Alignment.Center)
+                        .clickable {
+                            content?.value?.let { num1 ->
+                                content2?.value?.let { num2 ->
+                                    val result = num1.toInt() * num2.toInt()
+                                    list.add("result:${result}")
+                                    MainActivity.navigation.changeCurrentPage(PageName.SECONDPAGE)
+                                }
+                            }
+                        })
+            }
+        }
+
     }
 
 }
 
 @Composable
-fun InputView(prev: String, content: MutableState<String>) {
+fun InputView(prev: String, content: MutableState<String?>) {
 
     Column(
         modifier = Modifier
             .wrapContentHeight()
             .fillMaxWidth()
-            .background(Color.White)
     ) {
-        Text(text = "${prev}${content.value}",
+        Text(text = "${prev}${content?.value?:""}",
             Modifier
                 .wrapContentWidth()
                 .height(20.dp)
-                .background(Color.White)
                 .clickable {
 
                 })
-        TextField(
-            value = content.value,
-            textStyle = TextStyle(color = Color.Black),
-            onValueChange = { it ->
-                if (it.matches(Regex("[0-9]*\\.?[0-9]*"))) {
-                    content.value = it
-                }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .background(Color.White),
-            label = { "Enter a number" }
-        )
+        Box(Modifier.padding(10.dp)) {
+            TextField(
+                value = content?.value?:"",
+                textStyle = TextStyle(color = Color.Black),
+                onValueChange = { it ->
+                    if (it.matches(Regex("[0-9]*\\.?[0-9]*"))) {
+                        content?.value = it
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .wrapContentHeight()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = MaterialTheme.colors.surface,
+                    focusedIndicatorColor = MaterialTheme.colors.primary,
+                    unfocusedIndicatorColor = MaterialTheme.colors.onSurface
+                ),
+                label = { "Enter a number" }
+            )
+        }
+
 
     }
 }
